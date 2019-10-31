ed3d217a0d7ba92fb8389885be27194f10fbadd7
Handik Yuwono <34450274+handikyuwono05@users.noreply.github.com>
2019-11-01 06:09:53 +0700

    Add files via upload

---
+++ StocksNMobius.m
@@ -0,0 +1,28 @@
+%% 1 yang seharusnya volatilitas, rerata, dan epsilon pada Wiener Process (Wt) berubah tiap waktu yha namanya modifan
+close all
+clear all
+clc
+
+
+
+%% 2 yang seharusnya didefine alpha,beta per kill B,A per time yha namanya modifan
+close all
+clear all
+clc
+
+A(1) = 1143000; % USSR
+B(1) = 1040000; % NAZI
+alpha = 184000; % USSR
+beta = 104000; % NAZI
+t(1) = 0;
+waktu(1) = 0;
+for i = 1:7*30
+    A(i+1) = A(i) - beta*B(i)/A(i);
+    B(i+1) = B(i) - alpha*A(i)/B(i);
+    if B(i)>A(i)
+        waktu(i) = t(i);
+    end
+    t(i+1) = t(i) + 1;
+end
+plot(t,A,'r',t,B,'k')
+legend('USSR','NAZI')
